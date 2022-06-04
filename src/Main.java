17483911c49d727e792397883ed9ee7e7521b5e8
Artemii Usov <artemij.usov@yandex.ru>
2022-06-04 18:31:53 +0300

    2 version

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        taskOne(); // 1 задание
+        taskTwo(); // 2 задание
+        taskThree(); // 3 задание
+    }
+    public static void taskOne() {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName +" "+firstName+" "+middleName;
+        System.out.println(fullName);
+    }
+
+    public static void taskTwo() {
+        String fullName = "Ivanov Ivan Ivanovich";
+        String s1 = fullName.toUpperCase();
+        System.out.println("Data full name: "+s1);
+    }
+
+    public static void taskThree() {
+        String fullName = "Иванов Семён Семёнович";
+        String s2 = fullName.replace("ё","е");
+        System.out.println(s2);
     }
 }
